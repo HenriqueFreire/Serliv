dae82bc543c762b43845cd398f4230044943e8c7
HenriqueFreire <henriquefreire@outlook.com.br>
2023-12-08 21:56:40 -0300

    Aula de Javascript 13 concluída

---
+++ Curso de Javascript/02-Revisão de lógica de programação/12_variaveis.js
@@ -0,0 +1,4 @@
+/*
+var -> ES5 -> escopo global
+let -> ES6 -> escopo de bloco
+*/
---
+++ Curso de Javascript/02-Revisão de lógica de programação/13_variaveis2.js
@@ -0,0 +1,17 @@
+/*
+var -> ES5 -> escopo global
+let -> ES6 -> escopo de bloco
+
+let não permite redeclaração de variavel com mesmo nome no meesmo bloco
+*/
+
+let variavel = 10
+variavel = 'texto'
+
+/*
+dinamincamente tipado -> a linguagem que define 
+qual o tipo de dado de acordo com valor atribuído a ela 
+*/
+
+const constante = 'valor fixo'
+// constantes não podem ter seu valor modificado após sua declaração
