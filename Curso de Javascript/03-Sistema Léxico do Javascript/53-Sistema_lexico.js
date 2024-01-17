b5497bcca9f1e9ec052e5a54c282d0deb4e3914c
HenriqueFreire <henriquefreire@outlook.com.br>
2024-01-17 13:21:11 -0300

    Módulo de Sistema léxico do Javascript concluído

---
+++ Curso de Javascript/03-Sistema Léxico do Javascript/53-Sistema_lexico.js
@@ -0,0 +1,14 @@
+console.log('Javascript funcionando 2024')
+
+/*
+let nome = 'Henrique'
+console.log(10 +20)
+*/
+
+console.log('qualquer coisa')
+
+// uma única linha
+
+let nome = "Henrique"; alert(nome)
+
+const hasAttribute = ''
