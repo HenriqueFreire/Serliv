bab6ff1023215fd8ebf51db0b6a315225d396e61
HenriqueFreire <henriquefreire@outlook.com.br>
2024-02-20 15:42:55 -0300

    Aula 90 concluída

---
+++ Curso de Javascript/06-Array/89-push_pop_shift_unshift_slice_splice.js
@@ -0,0 +1,19 @@
+let arr = [1, 3, 5, 7, 9]
+
+arr.push(13)
+console.log(arr)
+
+arr.pop()
+console.log(arr)
+
+arr.shift()
+console.log(arr)
+
+arr.unshift(1)
+console.log(arr)
+
+let arr2 = arr.slice(2, 4)
+console.log(arr2)
+
+let arr3 = arr.splice(2)
+console.log(arr3)
---
+++ Curso de Javascript/06-Array/90-reverse_reduce.js
@@ -0,0 +1,9 @@
+let arr = [1, 2, 3]
+console.log(arr.reverse())
+
+let j = 0
+let soma = arr.reduce(function(acumulador, atual, index, _arr) {
+  console.log('j: ', j++)
+  return acumulador + elemento
+})
+console.log(soma)
